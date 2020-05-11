4aa0558b97eb4c78235ad1602616425808947a5e
Amantur <amantur2013@inbox.ru>
2020-05-11 23:17:53 +0600

    preloader

--- desing/js/main.js
+++ desing/js/main.js
@@ -1,4 +1,13 @@
+document.body.onload = function () {
+    setTimeout(function() {
+        var preloader = document.getElementById('preloader');
+        if (!preloader.classList.contains('done')){
+            preloader.classList.add('done');
+        }
+    }, 1000);
 
+
+};
 
 const tabs = document.querySelectorAll('.tab');
 
